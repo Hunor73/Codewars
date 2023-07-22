cfe83646e36935009279664755e255a1ee39a416
Hunor <hunor.peter@gmail.com>
2023-07-22 22:17:11 +0200

    Javascript update

--- 8kyu/price-of-mangoes.js
+++ 8kyu/price-of-mangoes.js
@@ -9,4 +9,16 @@ mango(3, 3) ==> 6    # 2 mangoes for $3 per unit = $6; +1 mango for free
 mango(5, 3) ==> 12   # 4 mangoes for $3 per unit = $12; +1 mango for free
 mango(9, 5) ==> 30   # 6 mangoes for $5 per unit = $30; +3 mangoes for free*/
 
-
+function mango(quantity, price){
+    console.log("Quantity:",quantity, "Price :",price)
+    //Number of sets (3 mangoes each)
+    const sets = Math.floor(quantity / 3)
+    console.log("Number of sets:",sets)
+    //Remaining mangoes outside the sets
+    const leftover =quantity %3
+    console.log("Remaining mangoes:",leftover)
+    //Total cost
+    const totalCost = (sets*2 + leftover) * price
+    console.log("Total cost:",totalCost)
+    return totalCost  
+}
