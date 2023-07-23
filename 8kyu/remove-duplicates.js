afeaa37d381e8f1d87e6027d42ee2a449ab1247d
Hunor <hunor.peter@gmail.com>
2023-07-23 18:24:51 +0200

    Git push

---
+++ 8kyu/remove-duplicates.js
@@ -0,0 +1,9 @@
+/*Title: Remove Duplicates from list
+Discription:
+Define a function that removes duplicates from an array of non negative numbers and returns it as a result.
+The order of the sequence has to stay the same.*/
+
+function distinct(a) {
+  return [...new Set(a)];
+  }
+//Codewars accepted. However, this function does not eliminate negatives.
