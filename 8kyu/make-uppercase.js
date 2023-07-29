2d2aa94602d85dee91d8b124a643ee6d53b1bc6b
Hunor <hunor.peter@gmail.com>
2023-07-29 07:58:48 +0200

    git commit

---
+++ 8kyu/make-uppercase.js
@@ -0,0 +1,9 @@
+/*
+Title: MakeUpperCase
+Description:
+Write a function which converts the input string to uppercase.
+*/
+function makeUpperCase(str) {
+    let capital = str.toUpperCase()
+    return capital
+  }
