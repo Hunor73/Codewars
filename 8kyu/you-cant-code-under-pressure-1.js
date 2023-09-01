df78e1992f2fe246dd126defa42ab92ca1f8c7f1
Hunor <hunor.peter@gmail.com>
2023-09-01 10:28:53 +0200

    You Can't Code Under Pressure #1

---
+++ 8kyu/you-cant-code-under-pressure-1.js
@@ -0,0 +1,10 @@
+/*
+Title: You Can't Code Under Pressure #1
+Description:
+Code as fast as you can! You need to double the integer and return it.
+*/
+function doubleInteger(i) {
+    i = i*2
+    return i
+}
+console.log(doubleInteger(8))
