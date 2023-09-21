f56cf72955c58f482441a82e4b4eaa46e773b19a
Hunor <hunor.peter@gmail.com>
2023-09-21 10:42:50 +0200

    Is it even?

---
+++ 8kyu/is-it-even.js
@@ -0,0 +1,24 @@
+/*
+Title: Is it even?
+Description: 
+In this Kata we are passing a number (n) into a function.
+Your code will determine if the number passed is even (or not).
+The function needs to return either a true or false.
+Numbers may be positive or negative, integers or floats.
+Floats with decimal part non equal to zero are considered UNeven for this kata.
+*/
+
+function testEven(n) {
+    if (n%2 ===0) {
+        return true
+    } else return false
+}
+
+console.log(testEven(6));
+
+/*
+Compact solution:
+function testEven(n) {
+    return n%2==0;
+}
+*/
--- 8kyu/isograms.js
+++ 8kyu/isograms.js
@@ -47,4 +47,16 @@ function isIsogram(str){
   return !str.match(/([a-z]).*\1/i);
 }
 
+It uses a regular expression to look for any repeated letters in the string and returns true if no repeated letters are found and false if any repeated letters are found. Here's how it works:
+
+    ([a-z]): This part of the regular expression matches any lowercase letter and captures it as a group. This is done using the [a-z] character class.
+
+    .*: This matches any number of characters (including letters) that come after the first matched letter.
+
+    \1: This backreference checks if the same letter as the one captured in the first group (([a-z])) appears later in the string.
+
+    i flag: The /i flag makes the regular expression case-insensitive, as mentioned in your initial problem statement.
+
+The match function returns null if no match is found, so the ! operator is used to invert the result. If a match is found (meaning there are repeated letters), it returns false, indicating that the string is not an isogram. If no match is found (no repeated letters), it returns true, indicating that the string is an isogram.
+
 */
