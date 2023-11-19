ac67bc0f791a4a0f06f693515a9331a0b310ea39
Hunor <hunor.peter@gmail.com>
2023-11-19 21:26:56 +0100

    String ends with?

---
+++ 7kyu/string-ends-with.js
@@ -0,0 +1,20 @@
+/*
+Title: String ends with?
+Desc: 
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+Examples:
+
+solution('abc', 'bc') // returns true
+solution('abc', 'd') // returns false
+
+*/
+
+function solution(str, ending){
+    let endLength = ending.length;
+    let strEnd = str.substring(str.length - endLength);
+    return strEnd === ending;
+}
+
+console.log(solution('abc', 'bc')); // should return true
+console.log(solution('abc', 'd'));  // should return false
