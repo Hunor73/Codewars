35aa811534f410901716784837a37f8780531a41
Hunor <hunor.peter@gmail.com>
2023-12-07 18:45:52 +0100

    Find the capitals

---
+++ 7kyu/find-the-capitals.js
@@ -0,0 +1,24 @@
+/*
+Title: Find the capitals
+Desc: 
+Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+Example (Input --> Output)
+
+"CodEWaRs" --> [0,3,4,6]
+
+*/
+
+var capitals = function (word) {
+    let UpperArray = [];
+	for (let i=0; i<word.length; i++ ) {
+        if (word[i] == word[i].toUpperCase())
+            UpperArray.push(i);
+    }
+    return UpperArray;
+};
+
+console.log(capitals("CodEWaRs"));
+
+/*
+
+*/
