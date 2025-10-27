cf844fefcbf796c0d5e231ab1ad001b3feeb7cb6
Hunor73 <hunor.peter@gmail.com>
2025-10-27 15:36:54 +0100

    Student's Final Grade

---
+++ 8kyu/CSharp/FinalGrade.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+
+namespace Codewars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Student's Final Grade
+             *Create a function finalGrade, which calculates the final grade of a student depending on two parameters: a grade for the exam and a number of completed projects.
+            This function should take two arguments: exam - grade for exam (from 0 to 100); projects - number of completed projects (from 0 and above);
+
+            This function should return a number (final grade). There are four types of final grades:
+
+                100, if a grade for the exam is more than 90 or if a number of completed projects more than 10.
+                90, if a grade for the exam is more than 75 and if a number of completed projects is minimum 5.
+                75, if a grade for the exam is more than 50 and if a number of completed projects is minimum 2.
+                0, in other cases
+
+            Examples(Inputs-->Output):
+
+            100, 12 --> 100
+            99, 0 --> 100
+            10, 15 --> 100
+
+            85, 5 --> 90
+
+            55, 3 --> 75
+
+            55, 0 --> 0
+            20, 2 --> 0
+
+            *Use Comparison and Logical Operators.            
+            */
+
+            Console.WriteLine(Kata.FinalGrade(100, 12));  // Output: 100
+            Console.WriteLine(Kata.FinalGrade(99, 0));   // Output: 100
+            Console.WriteLine(Kata.FinalGrade(10, 15));  // Output: 100
+
+            Console.WriteLine(Kata.FinalGrade(85, 5));   // Output: 90
+
+            Console.WriteLine(Kata.FinalGrade(55, 3));   // Output: 75
+
+            Console.WriteLine(Kata.FinalGrade(55, 0));   // Output: 0
+            Console.WriteLine(Kata.FinalGrade(20, 2));   // Output: 0
+        }
+
+        public static class Kata
+        {
+            public static int FinalGrade(int exam, int projects)
+            {
+                if (exam > 90 || projects > 10)
+                {
+                    return 100;
+                }
+                else if (exam > 75 && projects >= 5)
+                {
+                    return 90;
+                }
+                else if (exam > 50 && projects >= 2)
+                {
+                    return 75;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+    
+    }
+}
