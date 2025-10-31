416f3ce54bd0b02d8273a0880f73be164028b2a1
Hunor73 <hunor.peter@gmail.com>
2025-10-31 15:00:58 +0100

    5 without numbers

---
+++ 8kyu/CSharp/5WithoutNumbers.cs
@@ -0,0 +1,28 @@
+﻿using System; // Math.Abs
+
+namespace Codewars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 5 without numbers !!
+             * Write a function that always returns 5
+
+            Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+            Good luck :) */
+
+            Console.WriteLine(Kata.UnusualFive());
+
+        }
+
+        class Kata
+        {
+            public static int UnusualFive()
+            {
+                return "false".Length;
+            }
+        }
+    }
+}
