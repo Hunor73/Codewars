2650b10813ad633325879c6fc2b019802af63262
Hunor73 <hunor.peter@gmail.com>
2025-11-08 10:26:23 +0100

    Remove exclamation marks

---
+++ 8kyu/CSharp/Removeexclamationmarks.cs
@@ -0,0 +1,24 @@
+﻿using System; // Math.Abs
+
+namespace Codewars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Remove exclamation marks
+             Write the function RemoveExclamationMarks which removes all exclamation marks from a given string. */
+
+            Console.WriteLine(Kata.RemoveExclamationMarks("Hello World!"));
+
+        }
+
+        public class Kata
+        {
+            public static string RemoveExclamationMarks(string s)
+            {
+                return s.Replace("!", "");
+            }
+        }
+    }
+}
