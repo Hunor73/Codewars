4c90a37b3d55ac40f32ad448834e50f6e36585cb
Hunor73 <hunor.peter@gmail.com>
2025-11-09 10:16:29 +0100

    Switch it Up!

---
+++ 8kyu/CSharp/SwitchItUp.cs
@@ -0,0 +1,43 @@
+﻿namespace Codewars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Switch it Up!
+             * 
+             * When provided with a number between 0-9, return it in words.  Note that the input is guaranteed to be within the range of 0-9.
+
+                Input: 1
+
+                Output: "One".
+
+                If your language supports it, try using a switch statement. */
+             
+
+
+            Console.WriteLine(Kata.SwitchItUp(1));
+        }
+    }
+
+    public class Kata
+    {
+        public static string SwitchItUp(int number)
+        {
+            return number switch
+            {
+                0 => "Zero",
+                1 => "One",
+                2 => "Two",
+                3 => "Three",
+                4 => "Four",
+                5 => "Five",
+                6 => "Six",
+                7 => "Seven",
+                8 => "Eight",
+                9 => "Nine",
+                _ => "Unknown number"
+            };
+        }
+    }
+}
